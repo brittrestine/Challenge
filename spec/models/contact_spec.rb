036313db433be8505bf9063b7f031a5e5a3dc4b2
Brittany Restine <brittrestine@gmail.com>
2017-09-20 14:05:17 -0500

    tests for model

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -8,7 +8,24 @@ require File.expand_path '../../spec_helper.rb', __FILE__
       it 'formats contact birthday' do
         expect(contact_birthday.birthday_format).to eq("May 20, 1974")
       end
+    end
 
+    describe "formating phone number for contacts" do
+      let(:contact_work_phone) { Contact.create!(work_phone: "805-888-8989") }
+      let(:contact_mobile_phone) { Contact.create!(mobile_phone: "777-888-8989") }
+      let(:contact_home_phone) { Contact.create!(home_phone: "999-888-8989") }
+
+      it 'formats work_phone' do
+        expect(contact_work_phone.phone_format(contact_work_phone.work_phone)).to eq("(805)888-8989")
+      end
+
+      it 'formats mobile_phone' do
+        expect(contact_mobile_phone.phone_format(contact_mobile_phone.mobile_phone)).to eq("(777)888-8989")
+      end
+
+      it 'formats work_phone' do
+        expect(contact_home_phone.phone_format(contact_home_phone.home_phone)).to eq("(999)888-8989")
+      end
     end
 
 
