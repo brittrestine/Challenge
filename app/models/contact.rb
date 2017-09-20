8a3a2510509f5a17d9d930ae7deefea2b57dee18
Brittany Restine <brittrestine@gmail.com>
2017-09-20 13:17:25 -0500

    setting up testing

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,41 +1,9 @@
 class Contact < ApplicationRecord
   validates :email, {uniqueness: true}
 
-  def small_image?
-    return true if small_image.present?
-  end
-
-  def large_image?
-    return true if large_image.present?
-  end
-
-  def home_phone?
-    return true if home_phone.present?
-  end
-
-  def mobile_phone?
-    return true if mobile_phone.present?
-  end
-
-   def work_phone?
-    return true if work_phone.present?
-  end
-
-  def address?
-    return true if street_address.present?
-  end
-
-  def birthday?
-    return true if birthdate.present?
-  end
-
   def birthday_format
     date = DateTime.parse(birthdate)
     date.strftime("%B %d, %Y")
-  end
-
-  def email?
-    return true if email.present?
   end
 
 end
